24b9783612f77817b019daf84608da2b87b3c18c
Hieu Vo <votruongtrunghieu@gmail.com>
2025-06-09 19:38:14 +0700

    update

--- customized/solidity_contracts/TLS_custom/call_contract.js
+++ customized/solidity_contracts/TLS_custom/call_contract.js
@@ -13,6 +13,7 @@ const rl = readline.createInterface({
 async function call(
     selected_function,
     host,
+    privateKey,
     deployedContractAbi,
     deployedContractAddress,
   ) {
@@ -27,7 +28,37 @@ async function call(
         const crt = await fas.readFile(path, 'utf8');
         rl.question("Type CNAME: ", async (cname) => {
           rl.question("Type IP: ", async (ip) => {
-            await contractInstance.methods.addObject(Buffer.from(crt, 'utf-8'), Buffer.from(cname, 'utf-8'), Buffer.from(ip, 'utf-8')).call();
+            const web3 = new Web3(host);
+            const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+
+            // Tạo instance contract
+            const contract = new web3.eth.Contract(deployedContractAbi, deployedContractAddress);
+
+            // Encode lời gọi hàm
+            const data = contract.methods.set(crt, cname, ip).encodeABI();
+
+            // Lấy nonce
+            const nonce = await web3.eth.getTransactionCount(account.address, "pending");
+
+            // Tạo raw transaction
+            const tx = {
+              chainId: 1337,
+              nonce: web3.utils.toHex(nonce),
+              to: deployedContractAddress,
+              data: data,
+              gas: "0x24A22", // hoặc tùy chỉnh
+              gasPrice: "0xFF", // Besu không cần cao
+              value: "0x0",
+            };
+
+            // Ký giao dịch
+            const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
+
+            // Gửi giao dịch
+            const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+            // Trả về thông tin giao dịch
+            console.log("receipt:" + receipt);
           });
         });
       });
@@ -50,9 +81,10 @@ function main() {
   const contractJsonPath = path.resolve(__dirname, "VerificationContract.json");
   const contractJson = JSON.parse(fs.readFileSync(contractJsonPath));
   const abi = contractJson.abi;
+  const pkey = "";
 
   rl.question("Type '1' for adding objects, '2' for getting objects:", (opt) => {
-    call(opt, "http://127.0.0.1:8545", abi, "0x3c9687d86a4a93b9106df5b1abcc3a83c9831ce1");
+    call(opt, "http://127.0.0.1:8545", pkey, abi, "0x3c9687d86a4a93b9106df5b1abcc3a83c9831ce1");
   });
 }
 
